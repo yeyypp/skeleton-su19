28cd816aedabdec8ce1537164870b9399c12f65a
shuai <18610188361@163.com>
2020-02-06 22:27:15 +0800

    lab01: Completed LeapYear.java

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -1,6 +1,6 @@
 /**
  * Class that determines whether or not a year is a leap year.
- * @author YOUR NAME HERE
+ * @author Shuai Ye 
  */
 public class LeapYear {
 
@@ -12,6 +12,13 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+
+   private static boolean isLeapYear(int year) {
+       if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+	  return true;
+	}
+	return false;
+   }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
